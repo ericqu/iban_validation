e6f51ec4db95898851863613bbe1ff4af3909fbb
Eric Quere <13007637+ericqu@users.noreply.github.com>
2024-12-05 12:16:52 +0100

    removed dead-code

--- polars_plugin/tests/pibans.py
+++ polars_plugin/tests/pibans.py
@@ -3,13 +3,15 @@ import polars as pl
 from expression_lib import process_ibans
 
 df = pl.DataFrame(
-        {"ibans":["AT611904300234573201", "CY17002001280000001200527600"]}
+        {"ibans":["AT611904300234573201", "CY17002001280000001200527600", "TOTOT"]}
 )
 print(df)
 
 res = df.with_columns(
-    validated=process_ibans('ibans')
-)
+    validated=process_ibans('ibans').str.split_exact(',',2)\
+        .struct.rename_fields(['valid_ibans', 'bank_id', 'branch_id'])
+).unnest('validated')
+
 print(res)
 
 print('OK')
