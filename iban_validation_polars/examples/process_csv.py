487df1db66128aee1ea682497a57c384e1c81eac
Eric Quere <13007637+ericqu@users.noreply.github.com>
2026-02-08 13:31:19 +0100

    improved generation of semi-realistic inputfile

--- iban_validation_polars/examples/process_csv.py
+++ iban_validation_polars/examples/process_csv.py
@@ -10,13 +10,9 @@ inputfile = r"iban_validation_rs/data/IBAN Examples.txt"
 generatedfile = r"iban_validation_polars/examples/test_file.csv"
 sample_size = 100_000_000
 
-# generate a csv file for testing
-df = pl.read_csv(inputfile).sample(sample_size, with_replacement=True)
-df.write_csv(generatedfile)
-print("writing to file complete")
-
-start = time.perf_counter()
-df = (
+def validate_file(generatedfile):
+    start = time.perf_counter()
+    df = (
     pl.scan_csv(generatedfile)
     .with_columns(
         validated=process_ibans("IBAN Examples")
@@ -25,15 +21,13 @@ df = (
     )
     .unnest("validated")
     # .sort(by="IBAN Examples", descending=True)
-)
-# trigger the processing
-print(df.collect(engine='streaming'))
-duration = time.perf_counter() - start
-print(f'process_ibans for {sample_size} took {duration:.6f}')
-
-# cleanup
-os.remove(generatedfile)
-
+    )
+    # trigger the processing
+    print(df.collect(engine='streaming'))
+    duration = time.perf_counter() - start
+    # cleanup
+    os.remove(generatedfile)
+    return duration
 
 def sample_with_condition(csv_file, sample_size, random_state=None):
     df = pl.read_csv(csv_file)
@@ -50,48 +44,17 @@ def sample_with_condition(csv_file, sample_size, random_state=None):
     # Just concatenate - already at sample_size, no extra memory
     return pl.concat([sampled_target, sampled_others], how="vertical").sample(n=sample_size, with_replacement=True)
 
-sample_with_condition(inputfile, sample_size).write_csv(generatedfile)
 
-start = time.perf_counter()
-df = (
-    pl.scan_csv(generatedfile)
-    .with_columns(
-        validated=process_ibans("IBAN Examples")
-        .str.split_exact(",", 2)
-        .struct.rename_fields(["valid_ibans", "bank_id", "branch_id"])
-    )
-    .unnest("validated")
-    # .sort(by="IBAN Examples", descending=True)
-)
-# trigger the processing
-print(df.collect(engine='streaming'))
-duration = time.perf_counter() - start
-print(f'process_ibans 80/20 for {sample_size} took {duration:.6f}')
-
-# cleanup
-os.remove(generatedfile)
-
-
-
+# generate a csv file for testing
 df = pl.read_csv(inputfile).sample(sample_size, with_replacement=True)
 df.write_csv(generatedfile)
 print("writing to file complete")
 
-start = time.perf_counter()
-df = (
-    pl.scan_csv(generatedfile)
-    .with_columns(
-        validated=process_ibans("IBAN Examples")
-        .str.split_exact(",", 2)
-        .struct.rename_fields(["valid_ibans", "bank_id", "branch_id"])
-    )
-    .unnest("validated")
-    # .sort(by="IBAN Examples", descending=True)
-)
-# trigger the processing
-print(df.collect(engine='streaming'))
-duration = time.perf_counter() - start
+duration = validate_file(generatedfile)
 print(f'process_ibans for {sample_size} took {duration:.6f}')
 
-# cleanup
-os.remove(generatedfile)
+sample_with_condition(inputfile, sample_size).write_csv(generatedfile)
+
+duration = validate_file(generatedfile)
+
+print(f'process_ibans 80/20 for {sample_size} took {duration:.6f}')
